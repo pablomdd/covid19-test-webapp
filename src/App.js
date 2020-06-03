3c0407b9af7c7fb4d4010aad76dd7c59eb4bbccd
pablomdd <pablo.mdd.007@gmail.com>
2020-06-03 12:01:13 -0500

    add react particles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import React from 'react';
-import { UserForm } from './components/UserForm';
-// import './App.css';
+import React from "react";
+import Particles from "react-particles-js";
+import { UserForm } from "./components/UserForm";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <UserForm/>
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <Particles
+          params={{
+            particles: {
+              number: {
+                value: 75,
+              },
+              size: {
+                value: 5,
+              },
+            },
+            interactivity: {
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+              },
+            },
+          }}
+        />
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          <UserForm />
+        </div>
+      </div>
     </div>
   );
 }
