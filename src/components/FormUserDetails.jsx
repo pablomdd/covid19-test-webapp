adeebf1a723fa3eb99985ca760b74b9f7df69d57
pablomdd <pablo.mdd.007@gmail.com>
2020-06-02 19:16:43 -0500

    Add User Form Details

--- src/components/FormUserDetails.jsx
+++ src/components/FormUserDetails.jsx
@@ -1,13 +1,67 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import Navbar, { AppBar } from "material-ui/AppBar";
+import TextField from "material-ui/TextField";
+import RaisedButton from "material-ui/RaisedButton";
 export class FormUserDetails extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Hey</h1>
-            </div>
-        )
-    }
+  continue = (e) => {
+    e.preventDefault();
+    this.props.nextStep();
+  };
+  back = (e) => {
+    e.preventDefault();
+    this.props.prevStep();
+  };
+  render() {
+    const { values, handleChange } = this.props;
+
+    return (
+      <MuiThemeProvider>
+        <>
+          <Navbar title="COVID-19 Test" />
+          <TextField
+            hintText="Enter your First Name"
+            floatingLabelText="First Name"
+            onChange={handleChange("firstName")}
+            defaultValue={values.firstName}
+          />
+          <br />
+          <TextField
+            hintText="Enter your Last Name"
+            floatingLabelText="Last Name"
+            onChange={handleChange("lastName")}
+            defaultValue={values.lastName}
+          />
+          <br />
+          <TextField
+            hintText="Enter your Email"
+            floatingLabelText="Email"
+            onChange={handleChange("email")}
+            defaultValue={values.email}
+          />
+          <br />
+          <RaisedButton
+            label="Back"
+            primary={true}
+            style={styles.button}
+            onClick={this.back}
+          />
+          <RaisedButton
+            label="Continue"
+            primary={true}
+            style={styles.button}
+            onClick={this.continue}
+          />
+        </>
+      </MuiThemeProvider>
+    );
+  }
 }
 
-export default FormUserDetails
+const styles = {
+  button: {
+    margin: 15,
+  },
+};
+
+export default FormUserDetails;
