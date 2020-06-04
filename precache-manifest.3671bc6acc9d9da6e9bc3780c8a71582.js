54ea10412a656ffcae4ca78ec106fcf126c172d3
pablomdd <pablo.mdd.007@gmail.com>
2020-06-03 23:30:31 -0500

    Updates

---
+++ precache-manifest.3671bc6acc9d9da6e9bc3780c8a71582.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c7ddd3b90762e1515fb78e2d74cc68a",
+    "url": "/covid19-test-webapp/index.html"
+  },
+  {
+    "revision": "eaf44dec04ed2b9c4e17",
+    "url": "/covid19-test-webapp/static/css/main.2f2d2786.chunk.css"
+  },
+  {
+    "revision": "64db8b027d7b0fe0d0ed",
+    "url": "/covid19-test-webapp/static/js/2.3921a755.chunk.js"
+  },
+  {
+    "revision": "5b8b49146ea7ff4abb4f22a9d28772fe",
+    "url": "/covid19-test-webapp/static/js/2.3921a755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaf44dec04ed2b9c4e17",
+    "url": "/covid19-test-webapp/static/js/main.e8629eea.chunk.js"
+  },
+  {
+    "revision": "2081325e8f80da60eddc",
+    "url": "/covid19-test-webapp/static/js/runtime-main.bd4cbdf2.js"
+  }
+]);
